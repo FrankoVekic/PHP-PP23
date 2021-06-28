e0a095cc5ae83add190ab6b7f7ca4950d442f016
Franko Vekić <franko.vekic@gmail.com>
2021-06-28 23:56:54 +0200

    Spiralna tablica, nije gotova

---
+++ PHP Spiralna tablica/index.php
@@ -0,0 +1,60 @@
+<?php 
+error_reporting(1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ciklička tablica - PHP</title>
+    <link rel="stylesheet" href="assets/css/foundation.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="naslov">
+      <h2>CIKLIČKA TABLICA</h2>
+    <div class="objasnjenje1">
+<p> Korisnik unosi dva polja: broj redova i broj <br>stupaca. 
+  Ispod postoji gumb "KREIRAJ<br> TABLICU". Nakon submita, pored navedene <br>
+forme se prikazuje tablica zatraženih dimenzija <br> 
+gdje su polja popunjena brojevima. Brojevi su <br>
+popunjeni sljedećom logikom:</p>
+    </div>
+<p class="objasnjenje2">
++ BROJ 1 SE NALAZI U DONJEM DESNOM KUTU 
+    <br>
++ POLJA TABLICE SE POPUNJAVAJU SPIRALNO <br> CIKLIČKI U KRUG U SMJERU KAZALJKE NA SATU
+  </p>
+</div>
+<div class="input"> INPUT </div>
+<div>
+<form action="" method="post" class="form" >
+  <div>
+    <label for="red" class="brojredaka">Broj redaka</label>
+      <input type="text" name="red" class="red">
+  </div>  
+  <div>
+  <label for="stupac" class="brojstupaca">Broj stupaca</label>
+  <input type="text" name="stupac" class="stupac">
+  </div>
+    <div>
+    <input type="submit" name="submit" class="button expand" value="KREIRAJ TABLICU"/>
+    </div>
+    </form>
+  </div>
+<div class="output"> OUTPUT </div>                 
+<?php 
+    include 'matrix.php';
+?>
+</div>  
+    
+ 
+
+
+<script src="assets/js/vendor.js"></script>
+<script src="assets/js/foundation.js"></script>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
---
+++ PHP Spiralna tablica/matrix.php
@@ -0,0 +1,66 @@
+<?php 
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+include_once 'funkcije.php';
+
+if(isset($_POST['submit'])){
+    $red = $_POST['red'];
+    $stupac = $_POST['stupac'];
+    $broj=1;
+    $minRed = 0;
+    $maxRed= $red -1;
+    $minStup = 0;
+    $maxStup = $stupac -1;
+    $tablica=[];
+    
+    while ($broj <= $red*$stupac){
+        for($n=$maxStup; $n >= $minStup; $n--){
+            $tablica[$maxRed][$n] = $broj++;
+        }
+        $maxRed--;
+        
+        if($broj > $red*$stupac) 
+        break;
+
+        for($m=$maxRed;$m>=$minRed; $m--){
+            $tablica[$m][$minStup] = $broj++;
+
+        }
+        $minStup++;
+        if($broj > $red*$stupac) 
+        break;
+
+        for($n=$minStup; $n<=$maxStup;$n++){
+            $tablica[$minRed][$n] = $broj ++;
+
+        }
+        $minRed++;
+        if($broj > $red*$stupac) 
+        break;
+        
+        for($m=$minRed; $m<= $maxRed; $m++){
+            $tablica[$m][$maxStup]=$broj++;
+        }
+        $maxStup --;
+    }
+}
+
+?>
+
+<table class="tablica">
+<?php 
+if(isset($_POST['submit'])){
+        for($m=0;$m<$red;)
+        {
+          echo '<tr>';
+            for ($n=0; $n < $stupac;) 
+            { 
+              echo '<td>'. $tablica[$m][$n] .'</td>';
+              $n++;
+            }
+            echo '</tr>';
+            $m++;
+        }  
+    }
+?>
+</table>
+<?php 
